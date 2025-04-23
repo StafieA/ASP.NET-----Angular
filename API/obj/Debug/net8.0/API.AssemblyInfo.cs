f812c0c4b15491079770b48d734fe0a7ca65aa45
Andrei Stafie <andreistafie@gmail.com>
2025-04-23 13:44:58 +0300

    Create a BaseApiController

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c58bfe693682c39c30bab921e2a015d0b374dd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a48fbc8b55fb86465db946031f0b60c63b3ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
