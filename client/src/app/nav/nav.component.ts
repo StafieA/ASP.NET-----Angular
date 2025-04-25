3f4334e1b7f118c98c6d37cbf21d9b00a4971568
Andrei Stafie <andreistafie@gmail.com>
2025-04-25 17:19:46 +0300

    Create a AccountService

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,8 +9,13 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  private accountService = inject(AccountService);
   model: any = {};
+
+  ngOnInit(): void {
+    this.accountService.login(this.model);
+  }
 
   login() {
     console.log(this.model);
