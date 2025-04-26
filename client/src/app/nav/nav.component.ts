7895b176ae529ad752aa0d351413466a7b915aaa
Andrei Stafie <andreistafie@gmail.com>
2025-04-26 18:56:11 +0300

    Use conditionals to show HTML content

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
@@ -32,5 +33,9 @@ export class NavComponent implements OnInit {
       },
       error: (error) => console.log(error),
     });
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
